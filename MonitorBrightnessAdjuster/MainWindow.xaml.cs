da1b1431059cbf2f74fcf09d2f5a260b068de1ef
fairy <me@ilyfairy.com>
2024-07-13 10:26:02 +0800

    feat: 鼠标滚轮更改亮度值, F5刷新

--- MonitorBrightnessAdjuster/MainWindow.xaml.cs
+++ MonitorBrightnessAdjuster/MainWindow.xaml.cs
@@ -1,10 +1,19 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace MonitorBrightnessAdjuster {
     public partial class MainWindow: Window {
         public MainWindow() {
             InitializeComponent();
             DataContext = new MainWindowViewModel();
+        }
+
+        private void Slider_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            if (sender is Slider slider)
+            {
+                slider.Value += e.Delta / 120;
+            }
         }
     }
 }
