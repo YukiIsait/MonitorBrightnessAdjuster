88f87400dda194f77c116c4239d28c442d42adb7
YukiIsait <yukiisait@outlook.jp>
2024-08-09 17:23:03 +0800

    Refactor: Optimize data structure

--- LibDisplayDataChannel/LibDisplayDataChannel.c
+++ LibDisplayDataChannel/LibDisplayDataChannel.c
@@ -1,29 +1,34 @@
 ﻿#include "Framework.h"
 #include "LibDisplayDataChannel.h"
 
-typedef struct MonitorEnumProcParamStruct {
-    BOOL countOnly;
+typedef struct DdcDataStruct {
     DWORD monitorCount;
     DWORD availableMonitorCount;
     LPHANDLE availablePhysicalMonitorHandles;
     LPPHYSICAL_MONITOR physicalMonitors;
+} DdcData, *PDdcData;
+
+typedef struct MonitorEnumProcParamStruct {
+    BOOL countOnly;
+    PDdcData ddcData;
 } MonitorEnumProcParam, *PMonitorEnumProcParam;
 
 static BOOL CALLBACK MonitorEnumProc(HMONITOR monitor, HDC dc, LPRECT rect, LPARAM data) {
-    MonitorEnumProcParam* param = (MonitorEnumProcParam*) data;
+    PMonitorEnumProcParam param = (PMonitorEnumProcParam) data;
+    PDdcData ddcData = param->ddcData;
     // 当前监视器数量
     DWORD monitorCount = 0;
     // 统计每个监视器句柄对应的多个物理监视器数量
     if (!GetNumberOfPhysicalMonitorsFromHMONITOR(monitor, &monitorCount)) {
         return FALSE;
     }
-    param->monitorCount += monitorCount;
+    ddcData->monitorCount += monitorCount;
     // 仅计数则直接返回
     if (param->countOnly) {
         return TRUE;
     }
     // 当前监视器数组位置
-    LPPHYSICAL_MONITOR currentPhysicalMonitors = &param->physicalMonitors[param->monitorCount - monitorCount];
+    LPPHYSICAL_MONITOR currentPhysicalMonitors = &ddcData->physicalMonitors[ddcData->monitorCount - monitorCount];
     // 获取对应的物理监视器句柄
     if (!GetPhysicalMonitorsFromHMONITOR(monitor, monitorCount, currentPhysicalMonitors)) {
         return FALSE;
@@ -38,7 +43,7 @@ static BOOL CALLBACK MonitorEnumProc(HMONITOR monitor, HDC dc, LPRECT rect, LPAR
                                  &currentBrightness,
                                  &maximumBrightness)) {
             // 记录支持亮度调节的监视器句柄
-            param->availablePhysicalMonitorHandles[param->availableMonitorCount++] = currentPhysicalMonitors[i].hPhysicalMonitor;
+            ddcData->availablePhysicalMonitorHandles[ddcData->availableMonitorCount++] = currentPhysicalMonitors[i].hPhysicalMonitor;
         }
     }
     return TRUE;
@@ -47,33 +52,36 @@ static BOOL CALLBACK MonitorEnumProc(HMONITOR monitor, HDC dc, LPRECT rect, LPAR
 void* DdcInitialize() {
     HANDLE processHeap = GetProcessHeap();
     // 初始化变量
-    PMonitorEnumProcParam param = (PMonitorEnumProcParam) HeapAlloc(processHeap, HEAP_ZERO_MEMORY, sizeof(MonitorEnumProcParam));
-    if (!param) {
-        DdcDestroy(param);
+    PDdcData ddcData = (PDdcData) HeapAlloc(processHeap, HEAP_ZERO_MEMORY, sizeof(DdcData));
+    if (!ddcData) {
+        DdcDestroy(ddcData);
         return NULL;
     }
     // 首次遍历获取监视器数量
-    param->countOnly = TRUE;
-    if (!EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, (LPARAM) param)) {
-        DdcDestroy(param);
+    MonitorEnumProcParam param = {
+        .countOnly = TRUE,
+        .ddcData = ddcData
+    };
+    if (!EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, (LPARAM) &param)) {
+        DdcDestroy(ddcData);
         return NULL;
     }
     // 初始化监视器数组和可用标记数组
-    param->physicalMonitors = (LPPHYSICAL_MONITOR) HeapAlloc(processHeap, 0, param->monitorCount * sizeof(PHYSICAL_MONITOR));
-    param->availablePhysicalMonitorHandles = (LPHANDLE) HeapAlloc(processHeap, 0, param->monitorCount * sizeof(HANDLE));
-    param->countOnly = FALSE;
-    param->monitorCount = 0;
-    param->availableMonitorCount = 0;
-    if (!param->physicalMonitors || !param->availablePhysicalMonitorHandles) {
-        DdcDestroy(param);
+    ddcData->physicalMonitors = (LPPHYSICAL_MONITOR) HeapAlloc(processHeap, 0, ddcData->monitorCount * sizeof(PHYSICAL_MONITOR));
+    ddcData->availablePhysicalMonitorHandles = (LPHANDLE) HeapAlloc(processHeap, 0, ddcData->monitorCount * sizeof(HANDLE));
+    ddcData->monitorCount = 0;
+    ddcData->availableMonitorCount = 0;
+    if (!ddcData->physicalMonitors || !ddcData->availablePhysicalMonitorHandles) {
+        DdcDestroy(ddcData);
         return NULL;
     }
     // 获取所有监视器以及是否可用的状态
-    if (!EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, (LPARAM) param)) {
-        DdcDestroy(param);
+    param.countOnly = FALSE;
+    if (!EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, (LPARAM) &param)) {
+        DdcDestroy(ddcData);
         return NULL;
     }
-    return param;
+    return ddcData;
 }
 
 void DdcDestroy(void* handle) {
@@ -81,19 +89,19 @@ void DdcDestroy(void* handle) {
         return;
     }
     HANDLE processHeap = GetProcessHeap();
-    PMonitorEnumProcParam param = (PMonitorEnumProcParam) handle;
-    DestroyPhysicalMonitors(param->monitorCount, param->physicalMonitors);
-    HeapFree(processHeap, 0, param->physicalMonitors);
-    HeapFree(processHeap, 0, param->availablePhysicalMonitorHandles);
-    HeapFree(processHeap, 0, param);
+    PDdcData ddcData = (PDdcData) handle;
+    DestroyPhysicalMonitors(ddcData->monitorCount, ddcData->physicalMonitors);
+    HeapFree(processHeap, 0, ddcData->physicalMonitors);
+    HeapFree(processHeap, 0, ddcData->availablePhysicalMonitorHandles);
+    HeapFree(processHeap, 0, ddcData);
 }
 
 bool DdcGetAvailableCount(void* handle, uint32_t* count) {
     if (!handle || !count) {
         return false;
     }
-    PMonitorEnumProcParam param = (PMonitorEnumProcParam) handle;
-    *count = param->availableMonitorCount;
+    PDdcData ddcData = (PDdcData) handle;
+    *count = ddcData->availableMonitorCount;
     return true;
 }
 
@@ -105,11 +113,11 @@ bool DdcGetBrightness(void* handle,
     if (!handle || !currentBrightness || !minimumBrightness || !maximumBrightness) {
         return false;
     }
-    PMonitorEnumProcParam param = (PMonitorEnumProcParam) handle;
-    if (monitorIndex >= param->availableMonitorCount) {
+    PDdcData ddcData = (PDdcData) handle;
+    if (monitorIndex >= ddcData->availableMonitorCount) {
         return false;
     }
-    HANDLE physicalMonitorHandle = param->availablePhysicalMonitorHandles[monitorIndex];
+    HANDLE physicalMonitorHandle = ddcData->availablePhysicalMonitorHandles[monitorIndex];
     return GetMonitorBrightness(physicalMonitorHandle, minimumBrightness, currentBrightness, maximumBrightness);
 }
 
@@ -117,10 +125,10 @@ bool DdcSetBrightness(void* handle, uint32_t monitorIndex, uint32_t brightness) 
     if (!handle) {
         return false;
     }
-    PMonitorEnumProcParam param = (PMonitorEnumProcParam) handle;
-    if (monitorIndex >= param->availableMonitorCount) {
+    PDdcData ddcData = (PDdcData) handle;
+    if (monitorIndex >= ddcData->availableMonitorCount) {
         return false;
     }
-    HANDLE physicalMonitorHandle = param->availablePhysicalMonitorHandles[monitorIndex];
+    HANDLE physicalMonitorHandle = ddcData->availablePhysicalMonitorHandles[monitorIndex];
     return SetMonitorBrightness(physicalMonitorHandle, brightness);
 }
